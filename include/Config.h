d7be80536943053cfb7e1d015f4fc9d61ebbe275
sergeibudyanskiy <sergeibudyanskiy@gmail.com>
2023-10-10 08:08:11 +0300

    Add template method

--- include/Config.h
+++ include/Config.h
@@ -14,6 +14,24 @@ public:
     void Write(const nlohmann::json &data);
     std::string Get(const std::initializer_list<std::string> &keys) const;
     void Set(const std::initializer_list<std::string> &keys, const std::any &value);
+    template <typename T>
+    void Add(const std::initializer_list<std::string> &keys, const T &value)
+    {
+        Trace trace(Level::DEBUG, __func__);
+
+        nlohmann::json data = Read();
+        nlohmann::json *part = &data;
+
+        for (const auto &key : keys)
+        {
+            (*part)[key];
+            part = &(*part)[key];
+        }
+        
+        *part = value;
+
+        Write(data);
+    }
 
 private:
     std::string file_;
